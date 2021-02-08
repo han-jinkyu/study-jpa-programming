51a0f5935e8dd4d958d00bd0970f76932c249cd7
Jinkyu Han <hjk1019@gmail.com>
2021-02-08 21:45:57 +0900

    '애플리케이션 개발'에 대한 내용 추가

--- src/main/java/com/jpa/study/JpaMain.java
+++ src/main/java/com/jpa/study/JpaMain.java
@@ -1,7 +1,58 @@
 package com.jpa.study;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
 public class JpaMain {
     public static void main(String[] args) {
+        // 엔티티 매니저 팩토리 생성
+        EntityManagerFactory emf = Persistence
+                .createEntityManagerFactory("jpabook");
+        // 엔티티 매니저 생성
+        EntityManager em = emf.createEntityManager();
+        // 트랜잭션 획득
+        EntityTransaction tx = em.getTransaction();
 
+        try {
+            tx.begin();
+            logic(em);
+            tx.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    private static void logic(EntityManager em) {
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("지한");
+        member.setAge(2);
+
+        // 등록
+        em.persist(member);
+
+        // 수정
+        member.setAge(20);
+
+        // 한 건 조회
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMember=" + findMember.getUsername() +
+                ", age=" + findMember.getAge());
+
+        // 목록 조회
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        System.out.println("members.size=" + members.size());
+
+        // 삭제
+        em.remove(member);
     }
 }
